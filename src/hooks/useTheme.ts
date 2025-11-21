841c65a7a3bf407f7b576f60b3af751b7c54ae7f
Diego Rojas <rrojas1296@gmail.com>
2025-11-20 20:09:06 -0500

    master: Edited theme hook

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -12,6 +12,7 @@ export const useTheme = create<ITheme>((set) => ({
   setTheme: (theme) => {
     document.documentElement.classList.remove("dark", "light");
     document.documentElement.classList.add(theme);
+    localStorage.setItem(THEME_KEY, theme);
     return set((state) => ({ ...state, theme }));
   },
 }));
